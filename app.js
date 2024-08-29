a4647c0ecf9448eaa53f136f497fc233776bd96b
Coo1PeR <coo2per@gmail.com>
2024-08-29 15:00:05 +0300

    feat: add empty patch method

--- app.js
+++ app.js
@@ -65,6 +65,22 @@ app.post('/api/v1/tours', (req, res) => {
   });
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'No tours found'
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>'
+    }
+  });
+});
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
