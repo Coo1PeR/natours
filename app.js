5bb385ac9e50cd4f7d7f3fe3da204bbf4f70e42b
Coo1PeR <coo2per@gmail.com>
2024-08-31 11:32:27 +0300

    feat: use morgan, add comments

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+// 1) MIDDLEWARES
+app.use(morgan('dev'));
 
 app.use(express.json());
 
@@ -19,9 +23,11 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// 2) ROUT HANDLERS
+
 const getAllTours = (req, res) => {
   console.log(req.requestTime);
-  
+
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
@@ -103,6 +109,8 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// 3) ROUTS
+
 app
   .route('/api/v1/tours')
   .get(getAllTours)
@@ -113,6 +121,8 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+// 4) START SERVER
 
 const PORT = 3000;
 app.listen(PORT, () => {
