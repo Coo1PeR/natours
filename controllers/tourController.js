d7c38d48be1552f7fc6dd90b978015ddb29abe6b
Coo1PeR <coo2per@gmail.com>
2024-09-01 19:32:13 +0300

    refactor: extract check id to the middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,18 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'No tours found'
+    });
+  }
+  next();
+};
+
 
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
@@ -21,13 +33,6 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
   const id = +req.params.id;
   const tour = tours.find(el => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'No tours found'
-    });
-  }
 
   res.status(200).json({
     status: 'success',
@@ -54,13 +59,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (+req.params.id > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'No tours found'
-    });
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -70,13 +68,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (+req.params.id > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'No tours found'
-    });
-  }
-
   res.status(204).json({
     status: 'success',
     data: null
