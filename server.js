adc0215d2db9087e9532c8d1d673270f0bb71cdc
Coo1PeR <coo2per@gmail.com>
2024-09-02 20:54:59 +0300

    feat: dev config

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: `${__dirname}/config.env` });
+
 const app = require('./app');
 
-const PORT = 3000;
+//console.log(process.env);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}...`);
 });
