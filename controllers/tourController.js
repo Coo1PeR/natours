540e8ae05fe9ec9d41d5a66df68f4163a283db47
Coo1PeR <coo2per@gmail.com>
2024-09-02 19:40:44 +0300

    feat: add additional method of checking name and price to tour post request

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,20 +4,27 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
-exports.checkID = (req, res, next, val) => {
-  try {
-    console.log(`Tour id is: ${val}`);
+exports.checkID = (req, res, next/*, val*/) => {
 
-    if (+req.params.id > tours.length) {
-      return res.status(404).json({
-        status: 'fail',
-        message: 'No tours found'
-      });
-    }
-    next();
-  } catch (err) {
-    next(err);
+  //console.log(`Tour id is: ${val}`);
+
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'No tours found'
+    });
   }
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price'
+    });
+  }
+  next();
 };
 
 
